1c07766bb7221e1e424e3eb2db5276fe07cbd48e
Robert Paterno <bobpaterno@gmail.com>
2014-06-24 10:06:59 -0500

    moved es6 main to es5

---
+++ app/static/js/es5/main.js
@@ -0,0 +1,22 @@
+/* global Game, Phaser */
+
+var game;
+
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'mothgame');
+
+    game.state.add('boot', Game.Boot);
+    game.state.add('load', Game.Load);
+    game.state.add('menu', Game.Menu);
+    game.state.add('play', Game.Play);
+    game.state.add('killmoth', Game.Killmoth);
+
+    game.state.start('boot');
+  }
+
+})();
